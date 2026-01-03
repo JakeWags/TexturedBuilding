846c19841a1bbc2c0e8db1f912ba8f49d8334001
JakeWags <jakewagoneredu@gmail.com>
2026-01-02 20:04:10 -0700

    Move settings

--- TexturedBuilding/TexturedBuildingModSystem.cs
+++ TexturedBuilding/TexturedBuildingModSystem.cs
@@ -6,16 +6,6 @@ using Vintagestory.API.Config;
 
 namespace TexturedBuilding
 {
-    public class TexturedBuildingSettings
-    {
-        public bool AllowFood { get; set; } = false;
-        public bool AllowPlants { get; set; } = false;
-        public bool AllowBlockEntities { get; set; } = false;
-        public bool AllowLiquids { get; set; } = false;
-        public bool AllowClay { get; set; } = false;
-        public bool DebugMode { get; set; } = false;
-    }
-
     public class TexturedBuildingModSystem : ModSystem
     {
         public TexturedBuildingSettings Settings { get; set; } = new();
---
+++ TexturedBuilding/TexturedBuildingSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexturedBuilding
+{
+    public class TexturedBuildingSettings
+    {
+        public bool AllowFood { get; set; } = false;
+        public bool AllowPlants { get; set; } = false;
+        public bool AllowBlockEntities { get; set; } = false;
+        public bool AllowLiquids { get; set; } = false;
+        public bool AllowClay { get; set; } = false;
+        public bool DebugMode { get; set; } = false;
+
+        public String Whitelist { get; set; } = "";
+
+        public String Blacklist { get; set; } = "";
+    }
+}
